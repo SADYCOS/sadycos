7e13739f4ce2460545367149dc625f15bfbe0788
Friedrich Tuttas <friedrich.tuttas@ifr.uni-stuttgart.de>
2025-02-28 18:06:20 +0100

    change gain search plotting function

--- UserFiles/Configurations/+ExampleMission/@GainSearch/GainSearch.m
+++ UserFiles/Configurations/+ExampleMission/@GainSearch/GainSearch.m
@@ -2,7 +2,7 @@ classdef GainSearch < ExampleMission.DefaultConfiguration
 
     methods (Static)
         function parameters_cells = configureParameters()
-            num_Kp_values = 5;
+            num_Kp_values = 7;
             Kp_values = 10.^linspace(-5,1,num_Kp_values);
             Kd_values = Kp_values;
             [Kp_mesh, Kd_mesh] = meshgrid(Kp_values, Kd_values);
@@ -42,29 +42,29 @@ classdef GainSearch < ExampleMission.DefaultConfiguration
     end
 
     methods (Access = public)
-        function fig = plotSettlingTime(obj)
+        function fig = plotRmsError(obj)
             num_simulations = numel(obj.parameters_cells);
-            settling_times = inf(num_simulations, 1);
-            kp = nan(size(settling_times));
+            rms_error = inf(num_simulations, 1);
+            kp = nan(size(rms_error));
             kd = kp;
             for index = 1:num_simulations
                 error_quaternion = obj.simulation_outputs(index).logsout{6}.Values.error_quaternion_RB;
-                [~, angles] = smu.unitQuat.rot.toAxisAngle(error_quaternion.Data');
-                settling_times_index = find(abs(rad2deg(angles)) > 1, 1, "last");
-                settling_times(index) = error_quaternion.Time(settling_times_index);
+                [~, angles] = smu.unitQuat.rot.toAxisAngle((sign(error_quaternion.Data(:,1)) .* error_quaternion.Data)');
+                duration = error_quaternion.Time(end) - error_quaternion.Time(1);
+                rms_error(index) = sqrt(trapz(error_quaternion.Time, angles'.^2) / duration);
 
                 kp(index) = obj.parameters_cells{index}.GncAlgorithms.QuaternionFeedbackControl.Kp;
                 kd(index) = obj.parameters_cells{index}.GncAlgorithms.QuaternionFeedbackControl.Kd;
             end
             fig = figure;
-            scatter(kp, kd, [], settling_times, 'filled');
+            scatter(kp, kd, [], rms_error, 'filled');
             cb = colorbar;
             xlabel('Proportional Gain');
             ylabel('Derivative Gain');
-            ylabel(cb, 'Settling Time in s');
+            ylabel(cb, 'RMS Angle Error in rad^2 s');
             yscale('log');
             xscale('log');
-            title('Settling Time During Gain Search');
+            title('RMS Angle Error During Gain Search');
         end
     end
 end
