7341a6892de10247e80500b08d4a8ebe24c69bbe
Paul-sat <78033605+Paul-sat@users.noreply.github.com>
2025-03-11 17:56:53 +0100

    added simple model for solar and lunar position, still open if they fully satisfy https://github.com/SADYCOS/sadycos/issues/10

---
+++ Core/ModelsLibrary/Environment/Emphemerides/SimpleSolarLunarPos/@SimpleSolarLunarPos/SimpleSolarLunarPos.m
@@ -0,0 +1,8 @@
+classdef SimpleSolarLunarPos < ModelBase
+    methods (Static)
+
+        [sun_position_SI_I__m,moon_position_MI_I__m] ...
+                                        = execute(time_current_mjd)
+        
+    end
+end
