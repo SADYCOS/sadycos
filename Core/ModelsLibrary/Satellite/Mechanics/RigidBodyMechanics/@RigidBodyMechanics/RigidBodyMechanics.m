354fe7ca10749c2010f99a6209c5313ac067e132
Niklas-Birkle <st173938@stud.uni-stuttgart.de>
2025-06-30 00:39:53 +0200

    remove parameter constructor in RigidBodyMechanics

--- Core/ModelsLibrary/Satellite/Mechanics/RigidBodyMechanics/@RigidBodyMechanics/RigidBodyMechanics.m
+++ Core/ModelsLibrary/Satellite/Mechanics/RigidBodyMechanics/@RigidBodyMechanics/RigidBodyMechanics.m
@@ -14,29 +14,4 @@ classdef RigidBodyMechanics < ModelBase
                         angular_velocity_BI_B__rad_per_s)
                         
     end
-
-    methods (Access = public)
-
-        function obj = RigidBodyMechanics(mass__kg, inertia_B_B__kg_m2)
-        % RigidBodyMechanics
-        %
-        %   Inputs:
-        %   mass__kg: Mass of the rigid body in kg
-        %   inertia_B_B__kg_m2: Inertia of the rigid body in the body frame in kg m^2
-        %
-
-        arguments
-            mass__kg (1,1) {mustBePositive}
-            inertia_B_B__kg_m2 (3,3) {mustBeSymmetricPositiveDefinite}
-        end
-
-            Parameters.mass__kg = mass__kg;
-            Parameters.inertia_B_B__kg_m2 = inertia_B_B__kg_m2;
-
-            %% Set Parameters in ModelBase Constructor
-            obj = obj@ModelBase("Parameters", Parameters);
-
-        end
-
-    end
 end
