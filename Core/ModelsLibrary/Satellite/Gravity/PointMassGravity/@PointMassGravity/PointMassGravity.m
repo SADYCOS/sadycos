e6ceee5446db5af1973c41ea6074b826b27ba38b
Niklas-Birkle <st173938@stud.uni-stuttgart.de>
2025-07-08 17:34:08 +0200

    Correct arguments of gravitational_force_I__N = execute() to only mass and ravitational_acceleration and adjust information comments

--- Core/ModelsLibrary/Satellite/Gravity/PointMassGravity/@PointMassGravity/PointMassGravity.m
+++ Core/ModelsLibrary/Satellite/Gravity/PointMassGravity/@PointMassGravity/PointMassGravity.m
@@ -2,9 +2,7 @@ classdef PointMassGravity < ModelBase
     methods (Static)
 
         gravitational_force_I__N = execute(mass__kg, ...
-                        inertia_B_B__kg_m2, ...
-                        gravitational_acceleration_I__m_per_s2,...
-                        ParametersPointMassGravity)
+                        gravitational_acceleration_I__m_per_s2)
         
     end
 
