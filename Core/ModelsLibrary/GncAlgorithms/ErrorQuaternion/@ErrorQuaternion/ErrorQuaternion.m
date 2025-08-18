84a8c5c14a29feccce24eb5806700cab92698b05
Friedrich Tuttas <friedrich.tuttas@ifr.uni-stuttgart.de>
2025-08-18 09:49:46 +0200

    implement error quaternion class

---
+++ Core/ModelsLibrary/GncAlgorithms/ErrorQuaternion/@ErrorQuaternion/ErrorQuaternion.m
@@ -0,0 +1,8 @@
+classdef ErrorQuaternion < ModelBase
+    methods (Static)
+
+        error_quaternion_RB = execute(reference_frame_attitude_quaternion_RI, ...
+                                        body_frame_attitude_quaternion_BI)
+
+    end
+end
