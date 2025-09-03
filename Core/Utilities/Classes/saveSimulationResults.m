c98cd6df0677fdf97dd44bdc6b1e6f1927e3e1c7
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2025-09-04 01:20:26 +0200

    Refactor saveSimulationResults function to ask user for folder selection and save simulation results with a timestamped filename.

--- Core/Utilities/Classes/saveSimulationResults.m
+++ Core/Utilities/Classes/saveSimulationResults.m
@@ -1,17 +1,27 @@
-function saveSimulationResults(obj, file_path)
-        % If no path given -> set standard
-        if nargin < 2 || isempty(file_path)
-            timestamp = datetime("now", "Format", "yyyyMMdd_HHmmss");
-            folder= "C:\Users\fn103921\Desktop\Birkle\Simulation_Results\";
-                if ~exist(folder, 'dir')
-                   mkdir(folder);
-                end
-            file_path = folder + string(timestamp) + ".mat";
-        end
+function saveSimulationResults(obj)
 
-        % save the simulation output object
+    % Ask user for folder
+    folder = uigetdir(pwd, 'Select folder to save simulation results');
+    
+    % Check if user canceled
+    if isequal(folder, 0)
+        disp('User canceled saving.');
+        return;
+    end
+
+      % Create a timestamp string: YYYYMMDD_HHMMSS
+    timestamp = datetime('now', 'Format', 'yyyyMMdd_HHmmss');
+    
+    % Define the full file path with timestamp
+    filename = fullfile(folder, ['simOut_' timestamp '.mat']);
+    
+    % Save the simOut variable
+    try
         simOut = obj.simulation_outputs;
-        save(file_path, 'simOut');
+        save(filename, 'simOut');
+        fprintf('Simulation results successfully saved to %s\n', filename);
+    catch ME
+        warning('Failed to save simulation results: %s', ME.message);
+    end
 
-        fprintf('SADYCOS >>> Simulation results saved to %s\n', file_path);
     end
