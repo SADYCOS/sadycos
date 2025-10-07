e8886aa3fe7245b7811a4b178d9db6b60b87dd9b
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2025-10-07 19:29:50 +0200

    Implement saveSimulation function with timestamp in filename.

--- Core/Utilities/Classes/@SimulationConfiguration/saveSimulation.m
+++ Core/Utilities/Classes/@SimulationConfiguration/saveSimulation.m
@@ -1,5 +1,22 @@
-function saveSimulation(obj)
+function saveSimulation(o)
 
-    arguments
-        obj (1,1) SimulationConfiguration
-    end 
+arguments
+    o SimulationConfiguration  % Ensure the input is of type SimulationConfiguration
+end
+
+struct_o = struct(o);
+
+% Ask for saving directory
+save_path = uigetdir;
+
+% Create timestamp string and filename
+timestamp = datestr(now, 'ddmmyyyy_HHMMSS');
+filename = fullfile(save_path, ['o_' timestamp '.mat']);
+
+% Save selected file to the specified location
+try
+        save(filename, 'struct_o');
+        fprintf('Simulation data successfully saved to %s\n', filename);
+catch ME
+    warning('Failed to save simulation data: %s', ME.message);
+end
