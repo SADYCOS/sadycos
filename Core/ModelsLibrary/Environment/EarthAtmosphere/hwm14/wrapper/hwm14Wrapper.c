64abf8dbf7ca6ed94bbc70111631ef03f74407be
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2025-11-18 16:08:26 +0100

    Remove hwm14Wrapper.c

--- Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/wrapper/hwm14Wrapper.c
+++
@@ -1,9 +0,0 @@
-#include "mex.hpp"
-#include "mexAdapter.hpp"
-#include "hwm14Wrapper.h"
-
-class HWM14Wrapper : public matlab::mex::Function { 
-public:
-    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
-        // Extract input arguments
-        
