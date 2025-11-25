644272a3d7734834ad19cf1dd9661845a9087cb4
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2025-11-25 18:54:50 +0100

    Refactor variable declarations in hwm14_mex.F90 to provide: -Variables for Matlab data -Variables to receive Matlab input/output -Variables that are passed to hwm14

--- Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/wrapper/hwm14_mex.f90
+++ Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/wrapper/hwm14_mex.f90
@@ -19,13 +19,30 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
   external  hwm14  
   
 
-! local variables
-  integer          :: dayOfYear
-  real(real64)     :: UTsec, alt_km, glat, glon, Ap
-  real(real64)     :: Wmer, Wzon
-  double precision :: tmp
-  mwPointer        :: out_ptr
-  double precision :: w_out(2)
+! Pointers to MATLAB data
+! Declaration of variables with typ mwPointer
+! Syntax:
+! Typ :: variable1, variable2, ...
+
+ mwPointer :: p_iyd, p_sec, p_alt, p_glat, p_glon
+ mwPointer :: p_stl, p_f107a, p_f107, p_ap, p_wout
+
+
+! Declaration of double-precision (64-bit float) Fortran variables to receive MATLAB input/output
+
+ double precision :: d_iyd, d_sec, d_alt, d_glat, d_glon
+ double precision :: d_stl, d_f107a, d_f107
+ double precision :: d_ap(2), d_w(2)
+
+
+! Declaration of Fortran variables for HWM14 call
+! Variables that are passed to hwm14 (in expected format)
+! iyd --> 32-bit integer
+! sec, alt, glat, glon, stl, f107a, f107, ap(2), w(2) --> 32-bit float
+
+ integer*4 :: iyd
+ real*4    :: sec, alt, glat, glon, stl, f107a, f107
+ real*4    :: ap(2), w(2) 
 
 
 !
