15d5c290e596cc1391496999d6b19b637026fee7
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2025-12-09 20:57:42 +0100

    Update hwm14_mex.f90 to use mxGetDoubles instead the outdated mxGetPr function

--- Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/wrapper/hwm14_mex.f90
+++ Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/wrapper/hwm14_mex.f90
@@ -10,8 +10,8 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
 
 
 ! function declarations out of the MEX_API
-  mwPointer mxGetPr, mxCreateDoubleMatrix
-  external  mxGetPr, mxCreateDoubleMatrix
+  mwPointer mxGetDoubles, mxCreateDoubleMatrix
+  external  mxGetDoubles, mxCreateDoubleMatrix
   external  mxCopyPtrToReal8, mxCopyReal8ToPtr, mexErrMsgTxt
   
 
@@ -70,53 +70,53 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
 ! ------------------------------------------------------------
 ! Syntax:
 ! prhs(n) pointer to the n-th MATLAB input 
-! mxGetPr(...) delivers pointer to the double data
+! mxGetDoubles(...) delivers pointer to the double data
 ! 1. p_xyz points to the location in MATLAB memory where the input data is located 
 ! 2. call mxCopyPtrToReal8(source, target variable, number of values) copy the double-values from matlab to fortran
 ! 3. xyz = real(d_xyz) for real*4 or xyz = int(d_xyz) for integer*4 convert Fortran double to hwm14 expected data typ
 
   ! iyd (INTEGER*4)
-   p_iyd = mxGetPr(prhs(1))
+   p_iyd = mxGetDoubles(prhs(1))
    call mxCopyPtrToReal8(p_iyd, d_iyd, 1)
    iyd = int(d_iyd)
 
   ! sec
-   p_sec = mxGetPr(prhs(2))
+   p_sec = mxGetDoubles(prhs(2))
    call mxCopyPtrToReal8(p_sec, d_sec, 1)
    sec = real(d_sec)
 
   ! alt
-   p_alt = mxGetPr(prhs(3))
+   p_alt = mxGetDoubles(prhs(3))
    call mxCopyPtrToReal8(p_alt, d_alt, 1)
    alt = real(d_alt)
 
   ! glat
-   p_glat = mxGetPr(prhs(4))
+   p_glat = mxGetDoubles(prhs(4))
    call mxCopyPtrToReal8(p_glat, d_glat, 1)
    glat = real(d_glat)
 
   ! glon
-   p_glon = mxGetPr(prhs(5))
+   p_glon = mxGetDoubles(prhs(5))
    call mxCopyPtrToReal8(p_glon, d_glon, 1)
    glon = real(d_glon)
 
   ! stl
-   p_stl = mxGetPr(prhs(6))
+   p_stl = mxGetDoubles(prhs(6))
    call mxCopyPtrToReal8(p_stl, d_stl, 1)
    stl = real(d_stl)
 
   ! f107a
-   p_f107a = mxGetPr(prhs(7))
+   p_f107a = mxGetDoubles(prhs(7))
    call mxCopyPtrToReal8(p_f107a, d_f107a, 1)
    f107a = real(d_f107a)
 
   ! f107
-   p_f107 = mxGetPr(prhs(8))
+   p_f107 = mxGetDoubles(prhs(8))
    call mxCopyPtrToReal8(p_f107, d_f107, 1)
    f107  = real(d_f107)
 
   ! ap(2) – 2-element vector
-   p_ap = mxGetPr(prhs(9))
+   p_ap = mxGetDoubles(prhs(9))
    call mxCopyPtrToReal8(p_ap, d_ap, 2)
    ap(1) = real(d_ap(1))
    ap(2) = real(d_ap(2))
@@ -142,7 +142,7 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
 if (nlhs .eq. 1) then
 
    plhs(1) = mxCreateDoubleMatrix(2, 1, 0)   ! 0 = mxREAL
-   p_wout  = mxGetPr(plhs(1))
+   p_wout  = mxGetDoubles(plhs(1))
 
    d_w(1) = dble(w(1))
    d_w(2) = dble(w(2))
