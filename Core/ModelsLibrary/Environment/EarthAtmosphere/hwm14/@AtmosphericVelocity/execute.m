50fcce029ca4e54554ca513889323e57f8bc7155
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2026-03-17 11:27:45 +0100

    Implement atmospheric_velocity function to calculate aerodynamic velocity using hwm14 model

--- Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/@AtmosphericVelocity/execute.m
+++ Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/@AtmosphericVelocity/execute.m
@@ -1,2 +1,14 @@
+function atmospheric_velocity ...
+            = execute()
+
 % execute - Calculate aerodynamic velocity using
 % hwm14 model and atmospheric corotation
+
+cd(fullfile("Core","ModelsLibrary","Environment","EarthAtmosphere", ...
+"hwm14","@AtmosphericVelocity","externalData"));
+
+w = hwm14(150, 12*3600, 400, -45, -85, 80);
+
+atmospheric_velocity = w + corotation;
+
+end
