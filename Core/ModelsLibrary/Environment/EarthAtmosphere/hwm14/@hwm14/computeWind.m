de35b04af275a1835a26e2fbb10df52190fc2d5c
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2026-05-27 21:59:48 +0200

    Update computeWind function to return wind velocity and check data folder existence

--- Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/@hwm14/computeWind.m
+++ Core/ModelsLibrary/Environment/EarthAtmosphere/hwm14/@hwm14/computeWind.m
@@ -1,5 +1,16 @@
-function computeWind(dayOfYear, UTsec, alt_km, glat, glon, Ap)
+function w = computeWind(dayOfYear, UTsec, alt_km, glat, glon, Ap)
+
+    % Determine the path to the hwm14ifc_mex function
+    thisFile = mfilename('fullpath');           % Get the full path of the current function
+    hwm14Root = fileparts(fileparts(thisFile)); % Go up two levels to reach the hwm14 root directory
+    dataPath  = fullfile(hwm14Root, 'bin', 'install', 'share', 'data', 'hwm14');
+
+    % Check if the data folder exists, if not, throw an error
+    if ~isfolder(dataPath)
+        error('hwm14:dataNotFound', ...
+              'HWM14 data folder not found at:\n%s', dataPath);
+    end
 
     % Compute the atmospheric wind velocity
-    hwm14ifc_mex(dayOfYear, UTsec, alt_km, glat, glon, Ap);
+    w = hwm14ifc_mex(dayOfYear, UTsec, alt_km, glat, glon, Ap, dataPath);
 end
