7e79af08eb3ba5379c97ed158ee1069f55fbd369
Niklas Birkle <st173938@stud.uni-stuttgart.de>
2026-09-15 14:12:19 +0200

    Remove old integration of the "vleo-aerodynamics-tool" submodule and add it again correct to the namespace folder

--- Core/external_namespaces/new-vleo-aerodynamics-core/+new_vleo_aerodynamics_core/show_mesh.m
+++
@@ -1,3 +0,0 @@
-function show_mesh(satellite, triangle_visbility, velocity__m_per_s)
-    MexGateway("Visualization.show_mesh", int32(satellite.handle_), single(triangle_visbility), velocity__m_per_s);
-end
